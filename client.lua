d92be03a7f2f07ecaa65f3e552798ecad7141760
DK-DonnieKongy <52999643+DK-DonnieKongy@users.noreply.github.com>
2019-07-28 11:38:16 +1000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,27 +1,77 @@
-local pedindex = {}
+Config = {}
+Config.IgnoreList = {
+"PLAYER",
+"GANG_1",
+"GANG_2",
+"GANG_9",
+"GANG_10",
+"AMBIENT_GANG_LOST",
+"AMBIENT_GANG_MEXICAN",
+"AMBIENT_GANG_FAMILY",
+"AMBIENT_GANG_BALLAS",
+"AMBIENT_GANG_MARABUNTE",
+"AMBIENT_GANG_CULT",
+"AMBIENT_GANG_SALVA",
+"AMBIENT_GANG_WEICHENG",
+"AMBIENT_GANG_HILLBILLY",
+"MERRYWEATHER",
+"SECURITY_GUARD",
+"PRIVATE_SECURITY",
+}
 
-function SetWeaponDrops() -- This function will set the closest entity to you as the variable entity.
-    local handle, ped = FindFirstPed()
-    local finished = false -- FindNextPed will turn the first variable to false when it fails to find another ped in the index
-    repeat 
-        if not IsEntityDead(ped) then
-                pedindex[ped] = {}
-        end
-        finished, ped = FindNextPed(handle) -- first param returns true while entities are found
-    until not finished
-    EndFindPed(handle)
+-- All Ped Group hashes outside this list of peds needs to not be counted. So we need to get this list of peds...
 
-    for peds,_ in pairs(pedindex) do
-        if peds ~= nil then -- set all peds to not drop weapons on death.
-            SetPedDropsWeaponsWhenDead(peds, false) 
-        end
-    end
+local IgnoreList = {} -- This is the target list...
+
+function SetTableHash()
+	for _,v in ipairs(Config.Ignorelist) do
+		local v = GetHashKey(v)
+		table.insert(IgnoreList, v)
+	end
 end
 
+function SetWeaponDrops()
+	-- Set Your Player as a Ped Entity to use its Coords in the ESX Function.
+	local iPlayerPed = GetPlayerPed(-1)
+	local iPlayerPedEntity = GetEntityCoords(iPlayerPed)
+	-- Target Peds as the array and 25 being the distance around the player.
+	local Target, 25 = ESX.Game.GetClosestPedReverse(iPlayerPedEntity, IgnoreList)
+	-- For each ped inside the Target array pulled from ESX.
+	for i=1, #Target, 1 do
+		-- Are we sure its a ped?
+		if IsEntityAPed(Target) then
+			-- If Entity is not dead then...
+			if not IsEntityDead(Target) then
+				-- Dont Drop Guns!
+				SetPedDropsWeaponsWhenDead(Target, false)
+				-- Now when they die, I want to make sure we tell the server they are not needed. "CLEAN UP ISLE 7"
+				if IsPedDeadOrDying(Target) then
+					if IsPedInAnyVehicle(Target, false) then
+						RemovePedElegantly(Target)
+					end
+				end
+			end
+		end
+	end			
+end	
 
 Citizen.CreateThread(function()
+	SetTableHash()
+	Wait(50)
     while true do
-        Citizen.Wait(250)
         SetWeaponDrops()
     end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(250)
+		if IgnoreList ~= nil then
+			for _,v in ipairs(IgnoreList) do
+				local v = group
+				SetRelationshipBetweenGroups(0, group, GetHashKey('PLAYER'))
+			end
+		else break
+		end
+	end
 end)
